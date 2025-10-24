5a23fac177e2348c305321c99c5d2fa387f5c818
Aniket Inamdar <asinamdar2024@gmail.com>
2025-10-24 22:46:52 +0400

    fixed indentation

---
+++ include/constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const double G = 6.67430 * 10e-11; // Gravitational constant
+
+#endif CONSTANTS_H // !CONSTANTS_H
--- src/main.c
+++ src/main.c
@@ -1,71 +1,81 @@
+// Standard headers
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#include <glad/glad.h>
-
-#include <GLFW/glfw3.h>
+// Math headers
+#include <math.h>
 #include <cglm/cglm.h>
-#include <stb_image.h>
+#include <limits.h>
 
-static void error_callback(int error, const char *description)
+// Utility headers
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <stdint.h>
+
+// OpenGL graphics related headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Custom headers
+#include "constants.h"
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    fputs(description, stderr);
+  glViewport(0, 0, width, height);
 }
 
-static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+void processInput(GLFWwindow *window)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-static void render(GLFWwindow *window)
+int main(int argc, char *argv[])
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_ACCUM_BUFFER_BIT);
-}
+  // Initialize GLFW
+  if (!glfwInit())
+  {
+    perror("Error initializing GLFW\n");
+    exit(EXIT_FAILURE);
+  }
 
-int main(void)
-{
-    GLFWwindow *window;
+  // Create window and OpenGL context
+  GLFWwindow *window = glfwCreateWindow(800, 600, "My GLFW Window", NULL, NULL);
+  if (!window)
+  {
 
-    glfwSetErrorCallback(error_callback);
-
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-    window = glfwCreateWindow(640, 480, "OpenGL Boilerplate", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        fprintf(stderr, "Failed to initialize GLAD\n");
-        exit(EXIT_FAILURE);
-    }
-
-    glfwSetKeyCallback(window, key_callback);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        render(window);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        // glfwWaitEvents();
-    }
-
-    glfwDestroyWindow(window);
-
+    perror("Error creating window\n");
     glfwTerminate();
-    exit(EXIT_SUCCESS);
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    perror("Error loading GLAD");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // Mainloop
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+
+    // Render commands
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Check, call events and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return EXIT_SUCCESS;
 }
